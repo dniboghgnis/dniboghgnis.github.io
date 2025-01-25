ce66ff2f7fb5c8241723a2547fbb3f04d7d9ce4b
Gobind <withgobind@gmail.com>
2025-01-25 19:19:36 +0700

    update port

--- flask_app.py
+++ flask_app.py
@@ -40,4 +40,6 @@ def about():
     return render_template('index.html', title=title)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os 
+    port = int(os.environ.get('PORT', 4000))
+    app.run(host='0.0.0.0', port=port, debug=True)
