105f68497ce4284ce4e702701a580896a2800d9b
Gobind <withgobind@gmail.com>
2025-01-26 04:41:31 +0700

    debug

--- src/news_utils.py
+++ src/news_utils.py
@@ -10,12 +10,24 @@ def get_top_news(period="1h", search_term="India"):
     news_object.clear()
 
     news_dataframe = pd.DataFrame.from_dict(news)
-    print(news_dataframe.columns)
-    news_dataframe['sanitized_link'] = news_dataframe['link'].map(lambda x: x.split('&ved')[0])
+    
+    # Debugging: Print the columns of the DataFrame
+    print("Columns in news_dataframe:", news_dataframe.columns)
+    
+    # Check if 'link' column exists
+    if 'link' in news_dataframe.columns:
+        news_dataframe['sanitized_link'] = news_dataframe['link'].map(lambda x: x.split('&ved')[0])
+    else:
+        print("Error: 'link' column not found in the DataFrame")
     
     # Convert datetime column to the desired format
-    news_dataframe['sanitized_datetime'] = news_dataframe['datetime'].astype(str).apply(
-        lambda x: datetime.strptime(x, '%Y-%m-%d %H:%M:%S.%f').strftime('%A, %d %B %Y, %I:%M %p')
-    )
+    if 'datetime' in news_dataframe.columns:
+        news_dataframe['sanitized_datetime'] = news_dataframe['datetime'].astype(str).apply(
+            lambda x: datetime.strptime(x, '%Y-%m-%d %H:%M:%S.%f').strftime('%A, %d %B %Y, %I:%M %p')
+        )
+    else:
+        print("Error: 'datetime' column not found in the DataFrame")
     
     return news_dataframe
+
+print(get_top_news().columns)
