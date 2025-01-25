1b8a5decdd3125b34ffeb382f43873b306c90378
Gobind <withgobind@gmail.com>
2025-01-25 20:36:49 +0700

    fix link to articles

--- flask_app.py
+++ flask_app.py
@@ -14,6 +14,7 @@ def get_top_news(period="1h", search_term="India"):
     news_object.clear()
 
     news_dataframe = pd.DataFrame.from_dict(news)
+    news_dataframe['link'] = news_dataframe['link'].map(lambda x: x.split('&ved')[0])
     return news_dataframe
 
 @app.route('/')
