09b011057baa534f6012aa4f5d626ebb92addb2d
Gobind <withgobind@gmail.com>
2025-01-25 19:35:35 +0700

    refactor

--- flask_app.py
+++ flask_app.py
@@ -17,29 +17,24 @@ def get_top_news(period="1h", search_term="India"):
     return news_dataframe
 
 @app.route('/')
-# def home():
-#     title = 'Welcome to Our Website!'
-#     python_version = sys.version
-#     news_df = get_top_news()
-#     news_html = news_df.to_html(classes='news-table', index=False)
-#     return render_template('index.html', title=title, python_version=python_version, news_html=news_html)
 
 def home():
     title = 'Welcome to Our Website!'
     python_version = sys.version
     news_df = get_top_news()
     news_data = news_df.to_dict(orient='records')
-    
     return render_template('index.html', title=title, python_version=python_version, news_data=news_data)
-
 
 
 @app.route('/about/')
 def about():
     title = 'About Us'
-    return render_template('index.html', title=title)
+    python_version = sys.version
+    news_df = get_top_news()
+    news_data = news_df.to_dict(orient='records')
+    return render_template('index.html', title=title,  python_version=python_version, news_data=news_data)
 
 if __name__ == '__main__':
     import os 
-    port = int(os.environ.get('PORT', 4000))
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
